8ccd61e346e2138fa16b6c54b934fe14a0deb4b6
Widiya Marin <widiyamarin@gmail.com>
2023-01-20 16:12:44 +0800

    update latihan python

---
+++ Python_DataProfessional/Exercise19.py
@@ -0,0 +1,15 @@
+#Tuple manipulasi
+
+# Fitur .count()
+print(">>> Fitur .count()")
+tuple_score = ('Budi', 'Sud', 'Budi', 'Budi', 'Budi', 'Sud', 'Sud')
+score_budi = tuple_score.count('Budi')
+score_sud = tuple_score.count('Sud')
+print(score_budi)
+print(score_sud) 
+
+# Fitur .index()
+print(">>> Fitur .index()")
+tuple_score = ('Budi','Sud','Budi','Budi','Budi','Sud','Sud')
+score_pertama_sud = tuple_score.index('Sud')+1
+print(score_pertama_sud)
